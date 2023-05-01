1488a4d4378ae3ec824058d41367b5451316baa0
jinseo <ghkdwlstj99@gmail.com>
2023-05-01 21:47:36 +0900

    모바일버전으로 크롬 열기

---
+++ Selenium_mobile.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+options = Options()
+
+mobile_emulation = {"deviceName": "iPhone SE"}
+
+options.add_experimental_option("mobileEmulation", mobile_emulation)
+
+driver = webdriver.Chrome("C:\Chrome_Driver\chromedriver.exe", chrome_options=options)
+
+driver.get("https://www.naver.com")
